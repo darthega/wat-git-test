6f4995d9494582f2d1189b458684822593a93c98
Daniel Hoyos <daniel.hoyos@boszdigital.com>
2013-07-09 14:32:47 -0500

    WAT-DH03 Clean a perfect class

--- DanielService.java
+++ DanielService.java
@@ -17,21 +17,5 @@ public class DanielService {
 	public void thePerfectMethod() {
 		return "Daniel is perfect!!! Hail Lord Daniel!!!"
 	}
-
-       public void setLength(int length){
-		this.length = length;
-        }	stem.out.println("Camilo: are you a man? " + this.camilo);
-	}
-
-	public String DiegoIsALittleMan(){
-		int i=50;
-				System.out.println("Camilo: are you a man? " + this.camilo);
-	}
-
-	public String DiegoIsALittleMan(){
-		int i=50;
-		i--;
-		return i+"mm" ; 
-	}
-
+	
 }
