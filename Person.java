7dd1e0d042d26f6022324a8c1b8e947efc64e2cf
Oscar Eduardo Cala w <oscar.cala@boszdigital.com>
2013-07-09 09:36:14 -0500

    Solving the issue #WAT-1add the Person class

---
+++ Person.java
@@ -0,0 +1,11 @@
+public class Person{
+   private int age;
+   private String name;
+   private String lastname;
+
+   public Person(int age,String name,String lastname){
+	this.age=age;
+        this.name=name;
+        this.lastname=lastname;
+   }
+}
